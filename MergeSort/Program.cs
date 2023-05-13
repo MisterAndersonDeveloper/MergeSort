a4bf23280ee050092752ec1205fa900ffd46fe45
ADorian94 <igigaberos@gmail.com>
2023-05-13 15:54:29 +0200

    Add project files.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MergeSort;
+
+int[] inputArray = new int[] { 12, 11, 13, 5, 6, 7, 0 };
+SortEngine.MergeSort(inputArray, 0, inputArray.Length - 1);
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    Console.Write($"{inputArray[i]} ");
+}
---
+++ MergeSort/SortEngine.cs
@@ -0,0 +1,81 @@
+﻿namespace MergeSort
+{
+    internal static class SortEngine
+    {
+        public static void MergeSort(int[] unsortedArray, int start, int end)
+        {
+            if (start < end)
+            {
+                int middle = start + (end - start) / 2;
+
+                MergeSort(unsortedArray, start, middle);
+                MergeSort(unsortedArray, middle + 1, end);
+
+                Merge(unsortedArray, start, middle, end);
+            }
+        }
+
+        private static void Merge(int[] unsortedArray, int start, int middle, int end)
+        {
+            int n1 = middle - start + 1;
+            int n2 = end - middle;
+
+            int[] first = CopySubArray(unsortedArray, start, n1);
+            int[] second = CopySubArray(unsortedArray, middle + 1, n2);
+
+            int i = 0, j = 0, k = start;
+
+            while (i < n1 && j < n2)
+            {
+                if (first[i] < second[j])
+                {
+                    unsortedArray[k] = first[i];
+                    i++;
+                }
+                else
+                {
+                    unsortedArray[k] = second[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                unsortedArray[k] = first[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                unsortedArray[k] = second[j];
+                j++;
+                k++;
+            }
+
+
+            #region Advanced approach
+            //while (i < n1 && j < n2)
+            //    unsortedArray[k++] = first[i] < second[j] ? first[i++] : second[j++];
+
+            //while (i < n1)
+            //    unsortedArray[k++] = first[i++];
+
+            //while (j < n2)
+            //    unsortedArray[k++] = second[j++];
+            #endregion
+        }
+
+        private static int[] CopySubArray(int[] originalArray, int startIndex, int subLength)
+        {
+            int[] subArray = new int[subLength];
+
+            for (int i = 0; i < subLength; i++)
+                subArray[i] = originalArray[startIndex + i];
+
+            return subArray;
+        }
+    }
+}
